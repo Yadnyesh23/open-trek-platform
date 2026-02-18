06a4a5f9586ba57d2734703d0187c9f5e5b50895
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-02-18 20:19:21 +0530

    Increased  limit for JSON  and URL-encoded data

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -27,7 +27,8 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json())
-
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Routes
 import healthCheckRoute from './routes/healthcheck.route.js'
