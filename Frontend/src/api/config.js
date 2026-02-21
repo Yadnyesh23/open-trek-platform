b306981d56df5c3222b507ccff4d99ec52ef8ce9
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-02-21 17:23:46 +0530

    updated config.js

--- Frontend/src/api/config.js
+++ Frontend/src/api/config.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5001/api';
+
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "https://open-trek-platform.onrender.com/api" || 'http://localhost:5001/api';
 
 export default API_BASE_URL;
